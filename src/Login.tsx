be526ebf2927b4273c9d2d1a451816542efb6364
Jin Junjie <jinjj@chinasatnet.com.cn>
2024-12-16 22:01:53 +0800

    update

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,7 +5,7 @@ function Login() {
   return (
     <form action='/api/login' method='POST'>
       <input type='text' name='usernameField' placeholder='username' />
-      <input type='password' name='passwordField' placeholder='username' />
+      <input type='password' name='passwordField' placeholder='password' />
       <input type='roomId' name='roomIdField' placeholder='roomId' />
       <input type='submit' />
     </form>
