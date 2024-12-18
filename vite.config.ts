2b9302dee55763088cf910288e3b9fcd4e85d3e6
Jin Junjie <jinjj@chinasatnet.com.cn>
2024-12-18 15:29:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html';
+// import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    createHtmlPlugin({
-      minify: true, // 可选：压缩 HTML
-      inject: {
-        // 在 <head> 中注入 manifest.json 的 <link> 标签
-        tags: [
+    VitePWA({
+      registerType: 'autoUpdate', // 注册更新模式方式  默认是autoUpdate，将会自动更新，其他还有prompt和skipWaiting
+      injectRegister: 'auto', // 控制如何在应用程序中注册ServiceWorker 默认值是 'auto' ，其他如：'inline' 则是注入一个简单的注册脚本，内联在应用程序入口点中
+      manifest: { // manifest.json 文件配置
+        name: 'Simple Private Group Chat',
+        short_name: 'spgc',
+        description: 'Simple Private Group Chat',
+        theme_color: '#ffffff',
+        start_url: "/login",
+        icons: [
           {
-            tag: 'link',
-            attrs: {
-              rel: 'manifest',
-              href: '/manifest.json',
-            },
+            src: 'favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
-        ],
-      },
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
+    // createHtmlPlugin({
+    //   minify: true, // 可选：压缩 HTML
+    //   inject: {
+    //     // 在 <head> 中注入 manifest.json 的 <link> 标签
+    //     tags: [
+    //       {
+    //         tag: 'link',
+    //         attrs: {
+    //           rel: 'manifest',
+    //           href: '/manifest.json',
+    //         },
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 })
